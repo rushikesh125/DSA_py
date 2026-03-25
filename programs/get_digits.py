6848b7552d7d490bd317d641dec8e52cde2efbab
rushikesh gaikwad <lolits2626@gmail.com>
2026-03-25 18:06:05 +0530

    is armstrong no. program

--- programs/get_digits.py
+++ programs/get_digits.py
@@ -1,1 +1,10 @@
-digit = int(input("Enter a digit"))
+digit = int(input("Enter a digit:"))
+num =digit
+result=0
+size = len(str(digit))
+while num>0:
+    last_digit = num%10
+    result += last_digit**size
+    num = num//10
+
+print(result)
