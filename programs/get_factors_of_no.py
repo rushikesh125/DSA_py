055d279940f374b33aefb422c7bce913e795442b
rushikesh gaikwad <lolits2626@gmail.com>
2026-03-26 12:01:14 +0530

    optimal approch for factors of number

--- programs/get_factors_of_no.py
+++ programs/get_factors_of_no.py
@@ -1,7 +1,9 @@
+from math import sqrt
 num = int(input("Enter a number:"))
 result = []
-for i in range(1,num//2):
-    if num % i==0:
+for i in range(1,int(sqrt(num))+1):
+    if num%i==0:
         result.append(i)
-result.append(num)
+        if(num//i != i):
+            result.append(num//i)
 print(result)
